9db95d7fb6c024432b176a38110ab2f9a1ac920b
Hritik Chouhan <97736648+gitHritik@users.noreply.github.com>
2025-09-07 12:22:03 +0530

    Update musicLibrary.py

--- musicLibrary.py
+++ musicLibrary.py
@@ -1,5 +1,5 @@
 music = {
-    "shark": "https://www.youtube.com/watch?v=zfglpSSzvqg",
+    "sharks": "https://www.youtube.com/watch?v=zfglpSSzvqg",
     "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
     "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
     "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21"
